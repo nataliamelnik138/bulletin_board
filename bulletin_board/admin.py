bc41adcf5789b5c2c0a693fed2a619fcbffd0247
Natalia Melnik <nataliamelnik138@gmail.com>
2023-12-26 18:45:57 +0600

    Добавлено отображение моделей в админ. панели

--- bulletin_board/admin.py
+++ bulletin_board/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from bulletin_board.models import Advertisement, Review
+
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'price', 'author', 'created_at',)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'created_at', 'advertisement', 'author', )

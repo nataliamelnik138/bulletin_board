052ff0d93a808ec6f4d5ecd948f8ee4e33224def
Natalia Melnik <nataliamelnik138@gmail.com>
2023-12-28 19:22:28 +0600

    Представления и urls для работы с объявлениями и отзывами

--- bulletin_board/views.py
+++ bulletin_board/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from bulletin_board.filters import AdFilter
+from bulletin_board.models import Ad, Comment
+from bulletin_board.paginator import AdPaginator
+from bulletin_board.permissions import IsAdmin, IsAuthor
+from bulletin_board.serliazers import (AdDetailSerializer, AdSerializer,
+                                       CommentSerializer)
+
+
+class AdCreateAPIView(generics.CreateAPIView):
+    """Эндпоинт создания объявления"""
+    serializer_class = AdDetailSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        new_ad = serializer.save()
+        new_ad.author = self.request.user
+        new_ad.save()
+
+
+class AdListAPIView(generics.ListAPIView):
+    """Эндпоинт просмотра списка объявлений"""
+    serializer_class = AdSerializer
+    pagination_class = AdPaginator
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = AdFilter
+
+    def get_queryset(self):
+        queryset = Ad.objects.all().order_by('-created_at')
+        return queryset
+
+
+class AdRetrieveAPIView(generics.RetrieveAPIView):
+    """Эндпоинт просмотра объявления"""
+    serializer_class = AdDetailSerializer
+    queryset = Ad.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class AdUpdateAPIView(generics.UpdateAPIView):
+    """Эндпоинт редактирования объявления"""
+    serializer_class = AdDetailSerializer
+    queryset = Ad.objects.all()
+    permission_classes = [IsAuthenticated, IsAuthor or IsAdmin]
+
+
+class AdDestroyAPIView(generics.DestroyAPIView):
+    """Эндпоинт удаления привычки"""
+    queryset = Ad.objects.all()
+    permission_classes = [IsAuthenticated, IsAuthor or IsAdmin]
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        ad_pk = self.kwargs.get('pk')
+        print(ad_pk)
+        queryset = Comment.objects.filter(ad=ad_pk)
+        return queryset
+
+    def perform_create(self, serializer, *args, **kwargs):
+        new_comment = serializer.save()
+        new_comment.author = self.request.user
+        ad_pk = self.kwargs.get('pk')
+        new_comment.ad = Ad.objects.get(pk=ad_pk)
+        new_comment.save()
+
+    def get_permissions(self):
+        if self.action == 'create' or self.action == 'list' or \
+                self.action == 'retrieve':
+            permission_classes = [IsAuthenticated]
+        elif self.action == 'update' or self.action == 'destroy' or \
+                self.action == 'partial_update':
+            permission_classes = [IsAuthenticated, IsAdmin | IsAuthor]
+        return [permission() for permission in permission_classes]
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -21,11 +20,12 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/admin/', admin.site.urls),
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('', include('users.urls')),
+    path('api/', include('users.urls', namespace='users')),
+    path('api/ads/', include('bulletin_board.urls', namespace='ads')),
 ]
 
 if settings.DEBUG:

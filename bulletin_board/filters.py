2741eed1126a9f1694754899ce84fb5c6f1bcb9c
Natalia Melnik <nataliamelnik138@gmail.com>
2023-12-28 19:19:09 +0600

    Описан AdFilter для поиска по объявлениям

---
+++ bulletin_board/filters.py
@@ -0,0 +1,14 @@
+import django_filters
+
+from bulletin_board.models import Ad
+
+
+class AdFilter(django_filters.rest_framework.FilterSet):
+    title = django_filters.CharFilter(
+        field_name="title",
+        lookup_expr="icontains",
+    )
+
+    class Meta:
+        model = Ad
+        fields = ("title",)
